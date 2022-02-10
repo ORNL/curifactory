47d4001a921c3b2a0acc2d9b24b98149d3962802
Martindale, Nathan <martindalena@ornl.gov>
2022-02-10 13:33:22 -0500

    Add additional FileRefCacher tests

--- test/examples/stages/cache_stages.py
+++ test/examples/stages/cache_stages.py
@@ -4,7 +4,7 @@ import os
 
 
 @cf.stage(None, ["output_paths"], [FileReferenceCacher])
-def filerefcacher_stage(record):
+def filerefcacher_stage_multifile(record):
     file_path = record.get_dir("my_files")
     my_file_list = [os.path.join(file_path, f"thing{num}") for num in range(5)]
 
@@ -17,3 +17,17 @@ def filerefcacher_stage(record):
         outfile.write("\n")
 
     return my_file_list
+
+
+@cf.stage(None, ["output_path"], [FileReferenceCacher])
+def filerefcacher_stage(record):
+    file_path = record.get_path("my_file")
+
+    with open(file_path, "w") as outfile:
+        outfile.write("Test file\n")
+
+    # spit out a separate file into the cache to help detect if the stage ran.
+    with open(os.path.join(record.manager.cache_path, "stage_ran"), "w") as outfile:
+        outfile.write("\n")
+
+    return file_path
--- test/test_caching.py
+++ test/test_caching.py
@@ -3,9 +3,10 @@ import json
 import os
 import pytest
 
-from stages.cache_stages import filerefcacher_stage
+from stages.cache_stages import filerefcacher_stage, filerefcacher_stage_multifile
 
 
+# TODO: necessary? configured_test_manager already does this
 @pytest.fixture()
 def clear_stage_run(configured_test_manager):
     ran_path = os.path.join(configured_test_manager.cache_path, "stage_ran")
@@ -23,13 +24,13 @@ def clear_stage_run(configured_test_manager):
 def test_filerefcacher_stores_multiple_paths(configured_test_manager, clear_stage_run):
     """FileReferenceCacher should correctly store a list of files in the saved json."""
     r = cf.Record(configured_test_manager, cf.ExperimentArgs(name="test"))
-    filerefcacher_stage(r)
+    filerefcacher_stage_multifile(r)
 
     argshash = r.args.hash
     expected_list = [
         os.path.join(
             configured_test_manager.cache_path,
-            f"test_{argshash}_filerefcacher_stage_my_files/thing{i}",
+            f"test_{argshash}_filerefcacher_stage_multifile_my_files/thing{i}",
         )
         for i in range(5)
     ]
@@ -37,21 +38,67 @@ def test_filerefcacher_stores_multiple_paths(configured_test_manager, clear_stag
     with open(
         os.path.join(
             configured_test_manager.cache_path,
-            f"test_{argshash}_filerefcacher_stage_output_paths.json",
+            f"test_{argshash}_filerefcacher_stage_multifile_output_paths.json",
         ),
         "r",
     ) as infile:
         filelist = json.load(infile)
         assert filelist == expected_list
 
-
-# def test_filerefcacher_stores_single_path():
-# pass
-
-
-def test_filerefcacher_shortcircuits():
-    pass
+        for filename in filelist:
+            assert os.path.exists(filename)
 
 
-def test_filerefcacher_runs_when_file_missing():
-    pass
+def test_filerefcacher_stores_single_path(configured_test_manager, clear_stage_run):
+    r = cf.Record(configured_test_manager, cf.ExperimentArgs(name="test"))
+    filerefcacher_stage(r)
+
+    argshash = r.args.hash
+    expected_path = os.path.join(
+        configured_test_manager.cache_path,
+        f"test_{argshash}_filerefcacher_stage_my_file",
+    )
+
+    with open(
+        os.path.join(
+            configured_test_manager.cache_path,
+            f"test_{argshash}_filerefcacher_stage_output_path.json",
+        ),
+        "r",
+    ) as infile:
+        filelist = json.load(infile)
+        assert filelist == expected_path
+
+
+def test_filerefcacher_shortcircuits(configured_test_manager, clear_stage_run):
+    """FileReferenceCacher should short-circuit if all files in the filelist already exist."""
+    r0 = cf.Record(configured_test_manager, cf.ExperimentArgs(name="test"))
+    filerefcacher_stage_multifile(r0)
+
+    ran_path = os.path.join(configured_test_manager.cache_path, "stage_ran")
+    assert os.path.exists(ran_path)
+    os.remove(ran_path)
+
+    r1 = cf.Record(configured_test_manager, cf.ExperimentArgs(name="test"))
+    filerefcacher_stage_multifile(r1)
+    assert not os.path.exists(ran_path)
+
+
+def test_filerefcacher_runs_when_file_missing(configured_test_manager, clear_stage_run):
+    """FileReferenceCacher should _not_ short-circuit if any of the files in the filelist are missing."""
+    r0 = cf.Record(configured_test_manager, cf.ExperimentArgs(name="test"))
+    filerefcacher_stage_multifile(r0)
+
+    ran_path = os.path.join(configured_test_manager.cache_path, "stage_ran")
+    assert os.path.exists(ran_path)
+    os.remove(ran_path)
+    os.remove(
+        os.path.join(
+            configured_test_manager.cache_path,
+            f"test_{r0.args.hash}_filerefcacher_stage_multifile_my_files/thing1",
+        )
+    )
+
+    r1 = cf.Record(configured_test_manager, cf.ExperimentArgs(name="test"))
+    filerefcacher_stage_multifile(r1)
+    assert os.path.exists(ran_path)
