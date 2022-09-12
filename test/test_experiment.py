01b28f02e656b99eaec753c095aa02b8ef1bcbe9
Martindale, Nathan <martindalena@ornl.gov>
2022-09-12 13:08:48 -0400

    Fix broken test cases
    I forgot that when I add new CLI flags, and the parallel run args are
    measured with negative indices, I have to update them in the tests.

--- test/test_experiment.py
+++ test/test_experiment.py
@@ -250,9 +250,10 @@ def test_parallel_calls_count_correct(
     mock_queue = mocker.patch("multiprocessing.Queue")
     mock_queue.return_value.get = lambda: [None, "success"]
     run_experiment("basic", ["params1", "params2"], parallel=3)
-    assert mock.call_args_list[0].kwargs["args"][-9] == ["0-1"]
-    assert mock.call_args_list[1].kwargs["args"][-9] == ["1-2"]
-    assert mock.call_args_list[2].kwargs["args"][-9] == ["2-3"]
+    # ensure index_range is correct
+    assert mock.call_args_list[0].kwargs["args"][-12] == ["0-1"]
+    assert mock.call_args_list[1].kwargs["args"][-12] == ["1-2"]
+    assert mock.call_args_list[2].kwargs["args"][-12] == ["2-3"]
     assert mock.call_count == 3
 
 
@@ -266,9 +267,10 @@ def test_parallel_calls_count_correct_limits_threads(
     mock_queue = mocker.patch("multiprocessing.Queue")
     mock_queue.return_value.get = lambda: [None, "success"]
     run_experiment("basic", ["params1", "params2"], parallel=4)
-    assert mock.call_args_list[0].kwargs["args"][-9] == ["0-1"]
-    assert mock.call_args_list[1].kwargs["args"][-9] == ["1-2"]
-    assert mock.call_args_list[2].kwargs["args"][-9] == ["2-3"]
+    # ensure index_range is correct
+    assert mock.call_args_list[0].kwargs["args"][-12] == ["0-1"]
+    assert mock.call_args_list[1].kwargs["args"][-12] == ["1-2"]
+    assert mock.call_args_list[2].kwargs["args"][-12] == ["2-3"]
     assert mock.call_count == 3
 
 
