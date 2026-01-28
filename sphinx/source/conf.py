477236d7e743f5e2c3c4d9b08af139a18e4e4149
Martindale, Nathan <martindalena@ornl.gov>
2026-01-28 14:41:59 -0500

    Update copyright in sphinx config

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -20,7 +20,7 @@ sys.path.insert(0, os.path.abspath("../../"))
 # -- Project information -----------------------------------------------------
 
 project = "Curifactory"
-copyright = "2022, UT Battelle, LLC"
+copyright = "2026, UT Battelle, LLC"
 author = "Nathan Martindale"
 
 
